03fa800080ebffe213fbef4238bbabf69179bf68
Clint Priest <micro-chalk@git.rxv.me>
2018-06-23 18:43:23 -0500

    Initial commit of MicroChalk, supports most of the basic features I wanted and some additional niceties.

---
+++ MicroChalk.d.ts
@@ -0,0 +1,41 @@
+// Type definitions for cpriest/MicroChalk
+// Project: cpriest/MicroChalk
+// Definitions by: Clint Priest <https://github.com/cpriest>
+
+// export module MicroChalk {
+
+declare namespace MicroChalk {
+	  type AnsiCode = number;
+	  type Alias = string | AnsiCode;
+
+	  type Aliases = {
+		[key: string]: Alias;
+	}
+
+	 type Options = {
+		/**
+		 * Provide Aliases to existing AnsiCodes or new AnsiCodes
+		 * 	@example: {
+		 * 		'R' : 'red', 	// Would make 'R' equivalent to 'red' (which = AnsiCode 1)
+		 * 		'pink': 207 ,	// Would make 'pink' equivalent to AnsiCode 207
+		 * 	}
+		 **/
+		aliases?: Aliases;
+
+		/**
+		 * Declare a function to pre-process any input string before color processing
+		 */
+		pre: (input: string) => string;
+
+		/**
+		 * Declare a function to post-process any output string before returning to caller
+		 * 	This is useful for sending any results directly to a logging function
+		 */
+		post: (output: string) => string;
+
+		 /**
+		  * An alternate resetCode, defaults to \e[m which
+		  */
+		resetCode: Alias;
+	}
+}
---
+++ index.js
@@ -0,0 +1,182 @@
+'use strict';
+
+const longForms = {
+	'black':   0,
+	'red':     1,
+	'green':   2,
+	'yellow':  3,
+	'blue':    4,
+	'magenta': 5,
+	'cyan':    6,
+	'white':   7,
+	'Black':   8,
+	'Red':     9,
+	'Green':   10,
+	'Yellow':  11,
+	'Blue':    12,
+	'Magenta': 13,
+	'Cyan':    14,
+	'White':   15,
+};
+
+module.exports = (() => {
+	let mc = function MicroChalk(strings, ...keys) {
+		const args  = [].slice.call(arguments, 1);
+		const parts = [strings.raw[0]];
+
+		for(let i = 1; i < strings.length; i++) {
+			parts.push(String(args[i - 1]));
+			parts.push(String(strings.raw[i]));
+		}
+
+		let input = parts.join('');
+
+		if(mc.pre && typeof mc.pre == 'function')
+			input = mc.pre(input);
+
+		let output = colorize(input);
+
+		if(mc.post && typeof mc.post == 'function')
+			output = mc.post(output);
+
+		return output;
+	};
+
+	/**
+	 * Colorizes the input string based on the { } brackets
+	 * @param {string} input
+	 * @returns {string}
+	 */
+	function colorize(input) {
+		// logInfo('-->\n\t' + input + '\n');
+		// let r      = /{(\S+)\s+([^{}]+?)}/g, m;
+		let r      = /{(\S+)\s+((?:[^{}]|(?:{[^{}]*})*)+)}/g, m;
+		let output = input;
+
+		// noinspection JSValidateTypes
+		while((m = r.exec(input)) !== null) {
+			// inspect(m);
+			let { open, close } = convert(m[1]);
+			output              = output.replace(m[0], open + m[2] + close);
+		}
+		if(input != output)
+			return colorize(output);
+		return unroll(output);
+	}
+
+	/**
+	 * Resolves ${desc} into an {Alias} or {AnsiCode}
+	 * @param {MicroChalk.Alias} desc
+	 *
+	 * @returns {MicroChalk.AnsiCode|MicroChalk.Alias}
+	 */
+	function resolve(desc) {
+		if(desc == undefined)
+			return desc;
+
+		while(mc.aliases[desc])
+			desc = mc.aliases[desc];
+
+		if(!isNaN(parseInt(desc)))
+			return parseInt(desc);
+
+		if(longForms[desc] == undefined)
+			return longForms['white'];
+		return longForms[desc];
+	}
+
+	/**
+	 * Returns the opening/closing ansi codes for the given ${input} elements
+	 * @param {string} input    The descriptive replacements
+	 * @return {object} Opening/Closing Ansi Codes
+	 */
+	function convert(input) {
+		// let mon = input == '^' || input == '^^';
+
+		while(mc.aliases[input])
+			input = String(mc.aliases[input]);
+
+		let [ fg, bg ] = input.split('.');
+		let [ fgCode, bgCode] = [ resolve(fg), resolve(bg) ];
+
+		// if(mon)
+		// 	inspect([input, fg, bg, fgCode, bgCode]);
+
+		let open = [ ], close = [ ];
+
+		if(fgCode != undefined) {
+			open.push(`\x1B[38;5;${fgCode}m`);
+			close.push('\x1B[38;5m');				// Note, this is an invalid sequence which is post-processed by unroll()
+		}
+
+		if(bgCode != undefined) {
+			open.push(`\x1B[48;5;${bgCode}m`);
+			close.push('\x1B[48;5m');				// Note, this is an invalid sequence which is post-processed by unroll()
+		}
+
+		return {
+			open:  open.join(''),
+			close: close.join(''),
+		};
+	}
+
+	/**
+	 * Parses the input string for ANSI codes, keeping track of 'last fg/bg' color and
+	 *    restoring those colors when an (invalid) \e[38;5m or \e[48;5m shows up
+	 * @param {string} input
+	 * @return {string}
+	 */
+	function unroll(input) {
+		let r     = /\x1b\[((?:38|48);[25]);?(\d*)m/g, m,
+			stack = {};
+
+		// noinspection JSValidateTypes
+		while((m = r.exec(input)) !== null) {
+			let [type, color] = m.slice(1);
+			if(!stack[type])
+				stack[type] = [];
+
+			if(color) {
+				stack[type].push(color);
+			} else {
+				stack[type].pop();
+
+				let reset = mc.resetCode;
+				if(stack[type].length)
+					reset = `\x1B[${type};${stack[type].slice(-1)}m`;
+
+				input = input.slice(0, m.index) + reset + input.slice(m.index + m[0].length);
+			}
+		}
+		return input;
+	}
+
+	/**
+	 * Sets options for the logger
+	 *
+	 * @param {MicroChalk.Options} opts
+	 */
+	function options(opts = { }) {
+		mc.aliases = opts.aliases || mc.aliases || {};
+		mc.pre     = opts.pre || mc.pre;
+		mc.post    = opts.post || mc.post;
+
+		if(opts.resetCode) {
+			// inspect(opts.resetCode);
+			if(opts.resetCode.indexOf('.') == -1)
+				opts.resetCode += '.black';
+			// inspect(opts.resetCode);
+			// inspect(convert(opts.resetCode));
+			mc.resetCode = (convert(opts.resetCode).open) || mc.resetCode;
+		}
+		return mc;
+	}
+	mc.options = options;
+	mc.resetCode = '\x1B[m';
+
+	// logInfo(mc`This {red is {blue a test} run}: ${1 + 1}.  {white Yep, {green we {yellow like} ${'{blue template}'} literals}!}`);
+	// logInfo(mc`{red This is an {green previous failure}: {}}`);
+
+	return mc;
+})();
+
