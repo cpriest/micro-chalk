e609cf09b2748ad5ccbcc7c113f23589ecef1e81
Clint Priest <micro-chalk@git.rxv.me>
2018-08-11 05:49:04 -0500

    Added copies of ANSI.js/Parser.JS to git docs

---
+++ docs/res/js/ANSI.js
@@ -0,0 +1,311 @@
+'use strict';
+
+let _;	// General Purpose Variable
+let {Parser} = require('./Parser');
+
+const longForms = {
+	'black':   0, 'Black': 8,
+	'red':     1, 'Red': 9,
+	'green':   2, 'Green': 10,
+	'yellow':  3, 'Yellow': 11,
+	'blue':    4, 'Blue': 12,
+	'magenta': 5, 'Magenta': 13,
+	'cyan':    6, 'Cyan': 14,
+	'white':   7, 'White': 15,
+};
+
+/**
+ * @type Object<String,String>
+ */
+const chalkAliases = {
+	// chalk aliases
+	gray:          'Black',
+	redBright:     'Red',
+	greenBright:   'Green',
+	yellowBright:  'Yellow',
+	blueBright:    'Blue',
+	magentaBright: 'Magenta',
+	cyanBright:    'Cyan',
+	whiteBright:   'White',
+	bgBlack:       '.black', bgBlackBright: '.Black',
+	bgRed:         '.red', bgRedBright: '.Red',
+	bgGreen:       '.green', bgGreenBright: '.Green',
+	bgYellow:      '.yellow', bgYellowBright: '.Yellow',
+	bgBlue:        '.blue', bgBlueBright: '.Blue',
+	bgMagenta:     '.magenta', bgMagentaBright: '.Magenta',
+	bgCyan:        '.cyan', bgCyanBright: '.Cyan',
+	bgWhite:       '.white', bgWhiteBright: '.White',
+};
+
+const ESC   = '\x1B' || 'Ɛ',
+	  CSI   = `${ESC}[`,
+	  FGC   = `38;5`,
+	  FGC24 = `38;2`,
+	  FG    = `${CSI}${FGC};`,
+	  FG24  = `${CSI}${FGC24};`,
+	  BGC   = `48;5`,
+	  BGC24 = `48;2`,
+	  BG    = `${CSI}${BGC};`,
+	  BG24  = `${CSI}${BGC24};`;
+
+const SgrAttributeCodes = {
+	'bold':          [1, 22],	// Bold
+	'bright':        [1, 22],	// Bold
+
+	'dim':           [2, 22],	// Dim
+
+	'italic':        [3, 23],	// Italic
+	'italicized':    [3, 23],	// Italic
+
+	'under':         [4, 24],	// Underlined
+	'underline':     [4, 24],	// Underlined
+	'underlined':    [4, 24],	// Underlined
+
+	'blink':         [5, 25],	// Blinking
+	'blinking':      [5, 25],	// Blinking
+
+	'inverse':       [7, 27],	// Inverse
+
+	'hidden':        [8, 28],	// Hidden
+	'hide':          [8, 28],	// Hidden
+
+	'strike':        [9, 29],	// Strikethrough
+	'strikethrough': [9, 29],	// Strikethrough
+	'strikeout':     [9, 29],	// Strikethrough
+};
+
+// The entire match is replaced with {value Match0}
+const patternAliases = {
+	'\\*([^\\*]*?)\\*': 'bold',			// Bold
+//	'\\:([^\\:]*?)\\:': 'dim',			// Dim
+//	'\\#([^\\#]*?)\\#': 'italic',		// Italic
+	'\\_([^\\_]*?)\\_': 'underline',	// Underlined
+//	'\\&([^\\&]*?)\\&': 'blink',		// Blinking
+//	'\\!([^\\!]*?)\\!': 'inverse',		// Inverse
+//	'\\=([^\\=]*?)\\=': 'hidden',		// Hidden
+	'\\~([^\\~]*?)\\~': 'strike',		// Strikethrough
+};
+
+const AnsiCSI = {
+	'FG': FG,
+	'BG': BG,
+};
+
+const Ansi24CSI = {
+	'FG': FG24,
+	'BG': BG24,
+};
+
+const defaultResetCode = 'White.black';
+
+/**
+ * Converts a given css color hex code to its ISO-8613-6 24 bit code
+ *
+ * @param {string} css
+ *
+ * @return {string}
+ */
+function cssColorToAnsi24(css) {
+	if(css.length === 3)
+		css = css[0] + css[0] + css[1] + css[1] + css[2] + css[2];
+
+	return parseInt(css.substr(0, 2), 16) + ';' + parseInt(css.substr(2, 2), 16) + ';' + parseInt(css.substr(4, 2), 16);
+}
+
+class ANSI extends Parser {
+	constructor() {
+		super();
+
+		this.longForms      = longForms;
+		this.aliases        = {};
+		this.resetCode      = defaultResetCode;
+		this.patternAliases = patternAliases;
+		this.attrCodes      = SgrAttributeCodes;
+
+		return this.proxy();
+	}
+
+	/**
+	 * Handle non-base Parser markup, such as *bold*, then pass on to parent
+	 *
+	 * @param {string} input   Input string from the template literal tag function
+	 *
+	 * @returns {string}       Resulting translation of the marked up input
+	 */
+	markup(input) {
+		let pattern = Object
+			.keys(this.patternAliases)
+			.join('|');
+
+
+		let output  = input,
+			entries = Object.entries(this.patternAliases),
+			max     = entries.length + 1;
+
+		let r = new RegExp(pattern, 'g'),
+			m;
+
+		// noinspection JSValidateTypes
+		while((m = r.exec(output)) !== null) {
+			for(let j = 1; j <= max; j++) {
+				if(m[j] !== undefined) {
+					// j - 1 is the index of the matching patternAlias, [1] is the value to the pattern key
+					let desc = Object.entries(this.patternAliases)[j - 1][1];
+
+					output = output.replace(m[0], `{${desc} ${m[j]}}`);
+
+					r.lastIndex -= m[0].length; // Backtrack to beginning of our match
+					r.lastIndex += desc.length + 3;	// Move forward the description length + 3 for {, space and first character of match
+					break;	// for loop
+				}
+			}
+		}
+
+		return super.markup(output);
+	}
+
+	/**
+	 * Returns the opening/closing ansi codes for the given ${desc} elements
+	 *
+	 * @param {string} desc      The descriptive replacements
+	 * @param {object} prevTypes  The previous enclosing types from the parent context
+	 *
+	 * @return {object} Opening/Closing Ansi Codes
+	 */
+	xlate(desc, prevTypes) {
+		while(this.aliases[desc])
+			desc = String(this.aliases[desc]);
+
+		if(chalkAliases[desc])
+			desc = chalkAliases[desc];
+
+		function nextColor(result) {
+			return ['FG', 'BG', '_'].find(
+				x => !(x in result.types),
+			);
+		}
+
+		let colorOpen  = '',
+			colorClose = '';
+
+		let result = desc
+			.split('.')
+			.reduce((result, desc) => {
+					let { SgrType, code } = this.resolve(desc);
+
+					if(SgrType === 'color') {
+						let next = nextColor(result);
+
+						if(next === '_')
+							throw `Extraneous color (${desc}) specified in ${desc}, only two colors may be declared per markup block.`;
+
+						result.types[next] = code !== undefined
+											 ? AnsiCSI[next] + code + 'm'
+											 : code;
+
+						return result;
+					} else if(SgrType === 'color24b') {
+						let next = nextColor(result);
+
+						if(next === '_')
+							throw `Extraneous color (${desc}) specified in ${desc}, only two colors may be declared per markup block.`;
+
+						result.types[next] = code !== undefined
+											 ? Ansi24CSI[next] + code + 'm'
+											 : code;
+
+						return result;
+					} else if(SgrType === 'attr') {
+						result.open += CSI + code[0] + 'm';
+						result.close += CSI + code[1] + 'm';
+
+						// For bold/dim, we also want to set the FG color upon close
+						if(code[1] == 22)
+							colorClose = prevTypes.FG;
+
+						return result;
+					}
+					throw `Unknown type (${SgrType}) in ANSI::xlate()`;
+				}, { types: {}, open: '', close: '' },
+			);
+
+		for(let [type, code] of Object.entries(result.types)) {
+			if(code === undefined) {
+				delete result.types[type];
+				continue;
+			}
+			colorOpen += result.types[type];
+		}
+
+		result.open  = colorOpen + result.open;
+		result.close = result.close + colorClose;
+
+		return result;
+	}
+
+	/**
+	 * Resolves ${desc} into an {Alias} or {AnsiCode}
+	 *
+	 * @param {MicroChalk.Alias} desc
+	 *
+	 * @returns {object}
+	 */
+	resolve(desc) {
+		if(desc == undefined)
+			return { SgrType: 'color', code: undefined };
+
+		while(this.aliases[desc])
+			desc = this.aliases[desc];
+
+		while(chalkAliases[desc]) {
+			desc = chalkAliases[desc];
+			if(desc[0] == '.')
+				desc = desc.substr(1);
+		}
+
+		if(!isNaN(parseInt(desc)))
+			return { SgrType: 'color', code: parseInt(desc) };
+
+		if((_ = desc.match(/^#([a-f\d]{6}|[a-f\d]{3})$/i)) !== null)
+			return { SgrType: 'color24b', code: cssColorToAnsi24(_[1]) };
+
+		if(this.attrCodes[desc])
+			return { SgrType: 'attr', code: this.attrCodes[desc] };
+
+		return { SgrType: 'color', code: this.longForms[desc] };
+	}
+
+	/**
+	 * Sets options for the logger
+	 *
+	 * @param {MicroChalk.Options} opts
+	 */
+	options(opts = {}) {
+		this.aliases        = opts.aliases || this.aliases || {};
+		this.patternAliases = opts.patternAliases || this.patternAliases || {};
+
+		if(opts.resetCode) {
+			switch(opts.resetCode.indexOf('.')) {
+				case -1:
+					opts.resetCode += '.black';
+					break;
+				case 0:
+					opts.resetCode = 'White' + opts.resetCode;
+					break;
+			}
+			this.resetCode = opts.resetCode || this.resetCode;
+		} else {
+			this.resetCode = defaultResetCode;
+		}
+		return super.options(opts);
+	}
+
+	/**
+	 * Returns <input> with escapes translated to Ɛ to reveal the ansi codes
+	 * @param {string} input
+	 * @return {string}
+	 */
+	reveal(input) { return input.replace(/\x1B/g, 'Ɛ');	}
+}
+
+module.exports = new ANSI();
---
+++ docs/res/js/Parser.js
@@ -0,0 +1,176 @@
+'use strict';
+
+/**
+ * This masks all backslash escaped characters by making them \zXX where XX is the ascii hex code for the escaped character.
+ *
+ * @param {string} input
+ *
+ * @return {string}
+ */
+function mask(input) {
+	return input.replace(/\\(.)/g, (s) => {
+		return '\\z' + s.charCodeAt(1)
+			.toString(16);
+	});
+}
+
+/**
+ * This is the complement of mask() and converts \zXX sequences to their ascii character and removes the backslash
+ *
+ * @param {string} input
+ *
+ * @return {string}
+ */
+function unmask(input) {
+	return input.replace(/\\z(..)/g, (s, ...match) => {
+		return String.fromCharCode(parseInt(match[0], 16));
+	});
+}
+
+class Parser {
+	constructor() {
+		this.resetCode = '';
+	}
+
+	proxy() {
+		// noinspection JSCheckFunctionSignatures
+		return new Proxy(this.TagParser, {
+			apply:                    (target, _this, args) => this.TagParser.apply(this, args),
+			construct:                (target, args) => { throw 'construct() trap called'; },
+			get:                      (target, key) => this[key],
+			set:                      (target, key, value) => this[key] = value || true,
+			deleteProperty:           (target, key) => delete this[key],
+			enumerate:                (target, key) => Object.entries(this),
+			ownKeys:                  (target, key) => Object.entries(this),
+			has:                      (target, key) => key in this,
+			defineProperty:           (target, key, desc) => Object.defineProperty(this, key, desc),
+			getOwnPropertyDescriptor: (target, key) => Object.getOwnPropertyDescriptor(this, key),
+		});
+	}
+
+	TagParser(strings, ...keys) {
+		const args  = [].slice.call(arguments, 1);
+		const parts = [strings.raw[0]];
+
+		for(let i = 1; i < strings.length; i++) {
+			parts.push(String(args[i - 1]));
+			parts.push(String(strings.raw[i]));
+		}
+
+		let input = parts.join('');
+
+		if(this.pre && typeof this.pre == 'function')
+			input = this.pre(input);
+
+		// Mask /\\[{}]/ with something else
+		let output = this.markup(
+			mask(input),
+		);
+
+		// Unmask entries and unescape any remaining escaped characters
+		output = unmask(output)
+			.replace(/\\(.)/g, '$1');
+
+		if(this.post && typeof this.post == 'function')
+			output = this.post(output);
+
+		return output;
+	}
+
+	/**
+	 * Break apart the input string based on the { } brackets and pass the bracket
+	 * description to xlate for open/close information, recurse as needed.
+	 *
+	 * @param {string} input   Input string from the template literal tag function
+	 *
+	 * @returns {string}       Resulting translation of the marked up input
+	 */
+	markup(input) {
+		let [output, entries] = this.extract(input);
+
+		return this.expand(output, entries, this.xlate(this.resetCode, []).types);
+	}
+
+	/**
+	 * Recursive expansion of extracted string entries until no further extracted strings remain
+	 *
+	 * @param {string} input		The modified input from extract() and as recursed by expand
+	 * @param {object[]} entries	The indexed string entries found by the extract() function
+	 * @param {object} prevTypes	The previous enclosing types from the parent context
+	 *
+	 * @return {string}
+	 */
+	expand(input, entries, prevTypes) {
+		let r      = /\x1A(\d+)/g,
+			output = input,
+			m;
+
+		// noinspection JSValidateTypes
+		while((m = r.exec(input)) !== null) {
+			let entry                  = entries[parseInt(m[1])];
+			let { types, open, close } = this.xlate(entry[0], prevTypes);
+
+			close += Object.keys(types)
+				.filter(x => x in prevTypes)
+				.reduce((close, x) => close + prevTypes[x], '');
+
+			output = output.replace(m[0],							// Replace match
+				this.expand(open + entry[1] + close,				// with re-processed string
+					entries, Object.assign({}, prevTypes, types),	// Passing in our previous types overlayed with our current types
+				),
+			);
+		}
+		return output;
+	};
+
+
+	/**
+	 * Extracts the smallest { } pair and stores it as an entry in an array, replacing the match with the entry index.
+	 *    This process repeats until there are no further bracket pairs
+	 *
+	 * @param {string} input
+	 * @return {[string, array]}
+	 */
+	extract(input) {
+		let r       = /{(\S+)\s([^{}]+)}/g,
+			entries = [],
+			m;
+
+		// noinspection JSValidateTypes
+		while((m = r.exec(input)) !== null) {
+			r.lastIndex = 0;
+			entries.push(m.slice(1, 3));
+			input = input.replace(m[0], String.fromCharCode(26) + (entries.length - 1));
+		}
+
+		return [input, entries];
+	}
+
+	/**
+	 * Returns the opening/closing ansi codes for the given ${input} elements
+	 *
+	 * @param {string} input      The descriptive replacements
+	 * @param {object} prevTypes  The previous enclosing types from the parent context
+	 *
+	 * @return {object} Opening/Closing Ansi Codes
+	 */
+	xlate(input, prevTypes) {
+		throw 'xlate() must be implemented by sub-class';
+	}
+
+	/**
+	 * Sets options for the logger
+	 *
+	 * @param {MicroChalk.Options} opts
+	 */
+	options(opts = {}) {
+		this.pre  = 'pre' in opts ? opts.pre : this.pre;
+		this.post = 'post' in opts ? opts.post : this.post;
+
+		return this;
+	}
+}
+
+module.exports = {
+	Parser
+};
